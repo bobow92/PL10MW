1699eb03f6faa098aed9f45598aee13f1d2e7fff
bobow92 <36917565+bobow92@users.noreply.github.com>
2018-06-24 23:02:00 +0200

    Add files via upload

--- blog_leslie/src/Model/ArticleModel.php
+++ blog_leslie/src/Model/ArticleModel.php
@@ -5,7 +5,7 @@ class ArticleModel extends Model
 {
     
     public function getAllArticles(){
-        $requete = "SELECT article.*, author.* FROM article LEFT JOIN author ON article.id_author = author.id_author ORDER BY date_publication DESC";
+        $requete = "SELECT DISTINCT  article.*, author.* FROM article LEFT JOIN author ON article.id_author = author.id_author ORDER BY date_publication DESC";
         $resultat = $this -> getDb() -> query($requete);
 
         $donnees = $resultat -> fetchAll(PDO::FETCH_ASSOC);  
@@ -16,6 +16,21 @@ class ArticleModel extends Model
         else{
             return $donnees;
         }
+    }
+    
+    public function getArticleById($id){
+       $requete = "SELECT * from article, author, comment where article.id_author = author.id_author and comment.id_article = article.id_article and article.id_article = ". $id;
+       $resultat = $this -> getDb() -> query($requete);
+
+       $donnees = $resultat -> fetchAll(PDO::FETCH_ASSOC);
+       
+       if (!$donnees) {
+           return false;
+       }
+       else{
+            return $donnees;
+       }
+
     }
 
     public function getArticleByCategory($category){
@@ -33,44 +48,16 @@ class ArticleModel extends Model
 
     }
 
-    public function getArticleById($id){
-       $requete = "SELECT * from article, author, comment where article.id_author = author.id_author and comment.id_article = article.id_article and article.id_article = ". $id;
-       $resultat = $this -> getDb() -> query($requete);
 
-       $donnees = $resultat -> fetchAll(PDO::FETCH_ASSOC);
-       
-       if (!$donnees) {
-           return false;
-       }
-       else{
-            return $donnees;
-       }
-
-    }
     public function deleteArticleById($id){
         return $this -> delete($id);
     }
+
     public function registerArticle($infos){
         return $this -> register($infos);
     }
     
-    public function getAuthorByArticle($auth){
-        // $requete ="SELECT * FROM article LEFT JOIN author ON article.id_author = author.id_author WHERE author.id_author = article.id_author";
-        // // $requete = "SELECT article. * FROM article LEFT JOIN author ON article.id_author WHERE author.id_author = :id_author";
-        // $resultat = $this -> getDB() -> prepare($requete);
-        // $resultat -> bindValue(':id_author', $auth, PDO::PARAM_STR);
-        // $resultat -> execute();
-    
-        // $resultat -> setFetchMode(PDO::FETCH_CLASS, 'Entity\article');
-    
-        // $donnees = $resultat -> fetchAll();
-    
-        // if(!$donnees){
-        //     return FALSE;
-        // }else{
-        //     return $donnees;
-        // }
-    }
+ 
     
 
 
--- blog_leslie/src/Model/AuthorModel.php
+++ blog_leslie/src/Model/AuthorModel.php
@@ -8,7 +8,18 @@ class AuthorModel extends Model
         return $this -> findAll();
     }
     public function getAuthorById($id){
-        return $this ->find($id);
+        $requete = "SELECT * from article, author, comment where author.id_author = article.id_author and comment.id_article = article.id_article and author.id_author = ". $id;
+        $resultat = $this -> getDb() -> query($requete);
+
+        $donnees = $resultat -> fetchAll(PDO::FETCH_ASSOC);  
+     
+        if(!$donnees){
+            return false;
+        }
+        else{
+            return $donnees;
+        }
+    
     }
     public function deleteAuthorById($id){
         return $this -> delete($id);
@@ -21,37 +32,56 @@ class AuthorModel extends Model
             ':name'=>$_POST['name'],
             ':firstname'=>$_POST['firstname'],
             ':email'=>$_POST['email'],
-            ':password'=>md5($_POST['password']),
-            ':hash_validation'=>md5($_POST['password']),
+            ':password'=>sha1($_POST['password']),
+            ':hash_validation'=>sha1($_POST['password']),
             ':birth_date'=>$_POST['birth_date'],
             ));
   
      }
      public function connexion(){
-        //session_start();  
-        session_start(); 
-        print_r($_SESSION); 
+         
+            if(!empty($_POST['email']) && !empty($_POST['password'])){
+            $email   = $_POST['email']; 
+            $password   = sha1($_POST['password']); 
+       
+            $requete = "SELECT * FROM author WHERE email = :email AND password = :password";
+            $resultat = $this -> getDb() -> prepare($requete);
+            $resultat -> execute(array(
+                ':email'=>$email,
+                ':password'=>$password)
+            );
 
-        $email   = $_POST['email']; 
-        $password   = $_POST['password'];
-        $hashedPass = md5($password); 
-     
-        // check if user exist in BDD 
+                if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+                    $rowcount = $resultat -> rowCount();
+                     if ($rowcount > 0) {
+                    $result = $resultat->fetch();
+                    $_SESSION['id_author'] = $result['id_author'];
+                    $_SESSION['name'] = $result['name'];
+                    $_SESSION['firstname'] = $result['firstname'];
+                    $_SESSION['power'] = $result['power'];
+                    header("Location: ".$url."../../author/affiche/".$_SESSION['id_author']);
+                    }
+                    else{
+                        $erreur3 = '<p style="color: red;!important">Mauvais Email ou Mot de passe</p>';
+                    }
 
-        $requete = "SELECT email, password FROM author WHERE email=:email AND password=:password ";
-        $resultat = $this -> getDb() -> prepare($requete);
-        $resultat -> execute(array(':email'=>$email, ':password'=>$hashedPass));
-        $count = $resultat -> rowCount (); 
+                }
 
-        // echo $count;
-
-        if ($count < 0) {
-            echo 'Welcome' . ' ' . $email; 
-            $_SESSION['email'] = $email;
-            header('Location: dashboard');
-            exit();
         }
+        else{
+            $erreur2 = '<p style="color: red;!important">Mauvais Identifiants. ( email ou mot de passe ) </p>';    
+        }
+        
+    }
 
+    public function deconnexion(){
 
+        if(isset($_SESSION['id_author'])){
+
+        session_start();
+        $_SESSION = array();
+        session_destroy();
+        header("Location: ".$url."connexion/connexion.html");
+        }
     }
 }
