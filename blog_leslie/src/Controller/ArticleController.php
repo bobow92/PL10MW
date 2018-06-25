e22fd8c4e2a1fd7f89155e9e962dc6be84f0fb7d
bobow92 <36917565+bobow92@users.noreply.github.com>
2018-06-25 12:31:32 +0200

    Add files via upload

--- blog_leslie/src/Controller/ArticleController.php
+++ blog_leslie/src/Controller/ArticleController.php
@@ -52,6 +52,78 @@ class ArticleController extends Controller
         
         return $this -> render('layout.html','/../base/category.html', $params);
     }
+
+
+    public function createArticle(){
+         $erreur1 = "";
+            $erreur2 = "";
+            $erreur3 = "";
+            $erreur4 = "";
+            $erreur0 = "";
+                if(isset($_POST['formarticle'])){
+
+                    $title_article  = htmlspecialchars($_POST['title_article']);
+                    $content = htmlspecialchars($_POST['content']);
+                    $category = htmlspecialchars($_POST['category']);
+                    $img_article = $_FILE['img_article'];
+                    echo("<pre>");
+                    var_dump($_POST);
+                    echo("</pre>");
+                    echo("<pre>");
+                    var_dump($_FILE);
+                    echo("</pre>");
+
+                    if (!empty($_POST['title_article']) || !empty($_POST['content']) || !empty($_POST['category']) || !empty($_FILE['img_article']))
+                    {
+
+                 
+                        $strtitle_article = strlen($title_article);
+                        $strcontent = strlen($content);
+                        
+                        if ($strtitle_article <= 255 || $strcontent <= 1500){
+
+                            if($email == $email2){
+
+                                if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                                    
+                                
+                                    if ($mdp == $mdp2) {
+
+                                        $erreur4 = '<p><strong style="color: red;!important">Votre compte a bien été crée.</strong></p>';
+                                        
+                                        $authors = $this -> getModel() -> registerArticle();
+                                    }
+                                    else{
+                                        $erreur3 = '<p><strong style="color: red;!important">vos mot de passe ne correspondent pas</strong></p>';
+                                    }
+                                }   
+                            }
+                            else{
+                                $erreur2 = '<p><strong style="color: red;!important">vos adresses email ne correspondent pas.</strong></p>';
+                            }
+                               
+                        }
+                        else{
+                            $erreur1 = '<p><strong style="color: red;!important">votre prénom ou nom dépasse 255 caractères.</strong></p>';
+                        }
+                    }
+
+                
+                }
+                else{
+                    $erreur0 = '<p><strong style="color: blue;!important">Le formulaire doit être rempli entièrement.</strong></p>';
+                }
+                $params = array(
+                                        'title' => 'inscription',
+                                        'erreur1' => $erreur1,
+                                        'erreur2' => $erreur2,
+                                        'erreur3' => $erreur3,
+                                        'erreur4' => $erreur4,
+                                        'erreur0' => $erreur0,
+                                        );
+                return $this -> render('template.html', '/../Base/admin.html', $params);
+              
+    }
 }
 
 //echo'<pre>';
